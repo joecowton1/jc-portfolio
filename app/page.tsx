26d75bfa979a3e0b15c44f0469bddab663b8371d
Your Name <your_email@example.com>
2025-01-03 12:59:43 +0000

    Fix typos

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         {`I’m a creative software engineer with a strong track record in people management, project delivery and mentoring.`}
       </p>
       <p className="mb-4 text-lg">
-        {`Over the past seven years I've worked for both multi-nationals and small agencies, focussing primarily on Typescript, React and Node for web UIs and infrastrcuture. Most recently I’ve specialised in improving developer experience, building tooling and design systems to bring efficency and simpicity to engineers in adjacent teams.`}      
+        {`Over the past seven years I've worked for both multinationals and small agencies, focussing primarily on Typescript, React and Node for web UIs and infrastructure. Most recently I’ve specialised in improving developer experience, building tooling and design systems to bring efficiency and simplicity to engineers in adjacent teams.`}      
       </p>
       <p className="mb-4 text-lg">
         {`Before working in software, I spent ten years touring the world as an electronic music producer, releasing numerous records and running a highly respected record label called Livity Sound.`}
@@ -18,3 +18,6 @@ export default function Page() {
     </section>
   )
 }
+
+
+
