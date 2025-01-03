1c29b69d2309c1ad5999b2b9c251d6f892454079
Your Name <your_email@example.com>
2025-01-03 19:29:47 +0000

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Joe Cowton',
+    template: '%s | Joe Cowton',
   },
   description: 'This is my portfolio.',
   openGraph: {
